dff078d3f9b45933fa665bf9b9581cde57b99336
Clement Odeh <odehclementonwu@gmail.com>
2025-10-03 11:25:12 +0100

    add library

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -6,29 +6,44 @@ class Book:
 
     def book_name(self):
         return f"The book I just took from the library is {self.title}, written by {self.author} "
-    
+
     def check_out(self):
         if self.checked == False:
             # self.checked = True
             return f"{self.title} is available"
         else:
             return f"{self.title} is not available, please check back later"
-        
+
 class Library:
     def __init__(self):
         self._books = []
     def add_book(self, tit):
         self._books.append()
         return f"{self.title} has been added"
-    
+
     def check_out_book(self, title):
         if self.checked == True:
             return f"{self.title} has been checked out"
         else:
             return f"{self.title} has not been checked out"
-        
-     def return_book(self):
-        if not che
 
-     def list_available_books(self):
+     def return_book(sel
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+library_management.py[+] [unix] (10:39 03/10/2025)                                    30,25 All
+-- INSERT --
 
