ad0adda08abd093fd3a9214afd05b3b4e3497241
Clement Odeh <odehclementonwu@gmail.com>
2025-10-06 09:54:58 +0100

    add library

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -15,13 +15,14 @@ class Library:
         for book in self._books:
             if book.title == title and not book._is_checked_out:
                 book._is_checked_out = True
-                return f"{book.title} has been checked out"
+                return
         return f"{title} is available"
 
     def return_book(self,title):
         for book in self._books:
             if book.title == title and book._is_checked_out:
                 book._is_checked_out = False
+                return
         return f"'{title}' was not checked out."
 
     def list_available_books(self):
